5747e9ee7cb388c6f6112524c9f596f42cf9ddbe
Silvio Marques <silvio.mms2016@gmail.com>
2020-03-27 11:17:20 -0300

    Connecting the registration page with the backend

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 import leftPlant from '../../assets/left_plant.png';
 import rightPlant from '../../assets/right_plant.png';
+import api from '../../services/api';
 import './styles.css';
 
 export default function Register() {
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [whatsapp, setWhatsapp] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");    
+    const [registered, setRegistered] = useState(false);
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        const data = { name, email, whatsapp, city, state };
+
+        try {
+            const response = await api.post('/ngos/new', data);
+
+            setId(response.data.id);
+            setRegistered(true);
+        } catch (err) {
+            console.error(`Something went wrong. Check out the details: ${err}`);
+        }
+    }
+
     return (
         <div className="register_container">
             <div className="decorations">
@@ -25,15 +49,22 @@ export default function Register() {
                         Return
                     </Link>
                 </section>
-                <form className="right_content">
-                    <input placeholder="NGO Name" />
-                    <input placeholder="NGO Phone/Whatsapp" />
-                    <input type="email" placeholder="NGO Email" />
+                <form className="right_content" onSubmit={handleRegister}>
+                    <input placeholder="NGO Name" value={name} onChange={e => setName(e.target.value)} />
+                    <input placeholder="NGO Phone/Whatsapp" value={whatsapp} onChange={e => setWhatsapp(e.target.value)} />
+                    <input type="email" placeholder="NGO Email" value={email} onChange={e => setEmail(e.target.value)} />
                     <div className="input_group">
-                        <input placeholder="NGO City" />
-                        <input placeholder="State" style={{ width: 80 }} />
+                        <input placeholder="NGO City" value={city} onChange={e => setCity(e.target.value)} />
+                        <input placeholder="State" style={{ width: 80 }} value={state} onChange={e => setState(e.target.value)} />
                     </div>
-                    <button className="btn red_btn" type="submit">Register</button>
+                    {
+                        registered ?
+                        <div className="access_info">
+                            <p>Your access ID is: <strong>{id}</strong></p>
+                            <p>Keep it safe :)</p>
+                        </div>
+                        : <button className="btn red_btn" type="submit">Register</button>
+                    }
                 </form>
             </div>
         </div>
